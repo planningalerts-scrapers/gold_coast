2eea5da3f160c7c185809e336e22743ad9460bc7
Matthew Landauer <mlandauer@gmail.com>
2019-05-20 15:27:35 +1000

    Extract method

--- scraper.rb
+++ scraper.rb
@@ -7,6 +7,23 @@ class GoldCoastScraper
     @agent = Mechanize.new
   end
 
+  def scrape_detail_page(url, scraper)
+    # Get application page with a referrer or we get an error page
+    page = agent.get(url, [], URI.parse(scraper.base_url))
+    data = scraper.scrape_detail_page(page)
+
+    record = {
+      "council_reference" => data[:council_reference],
+      # Throw away the first part of the address which contains lot number
+      "address" => data[:address].split(", ")[1..-1].join(", "),
+      "description" => data[:description],
+      "info_url" => scraper.base_url,
+      "date_scraped" => Date.today.to_s,
+      "date_received" => data[:date_received]
+    }
+    EpathwayScraper.save(record)
+  end
+
   def applications
     scraper = EpathwayScraper::Scraper.new("https://cogc.cloud.infor.com/ePathway/epthprod")
     page = scraper.pick_type_of_search(:advertising)
@@ -22,20 +39,7 @@ class GoldCoastScraper
       end
     end
     urls.map do |url|
-      # Get application page with a referrer or we get an error page
-      page = agent.get(url, [], URI.parse(scraper.base_url))
-      data = scraper.scrape_detail_page(page)
-
-      record = {
-        "council_reference" => data[:council_reference],
-        # Throw away the first part of the address which contains lot number
-        "address" => data[:address].split(", ")[1..-1].join(", "),
-        "description" => data[:description],
-        "info_url" => scraper.base_url,
-        "date_scraped" => Date.today.to_s,
-        "date_received" => data[:date_received]
-      }
-      EpathwayScraper.save(record)
+      scrape_detail_page(url, scraper)
     end
   end
 end
