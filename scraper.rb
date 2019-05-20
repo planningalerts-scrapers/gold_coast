e69760aeaff109982f9e1834551a1c9cf3161c24
Matthew Landauer <mlandauer@gmail.com>
2019-05-20 14:40:24 +1000

    Move method to library

--- scraper.rb
+++ scraper.rb
@@ -23,27 +23,15 @@ class GoldCoastScraper
     "https://cogc.cloud.infor.com/ePathway/epthprod/Web/GeneralEnquiry/EnquiryLists.aspx?ModuleCode=LAP"
   end
 
-  def extract_total_number_of_pages(page)
-    page_label = page.at('#ctl00_MainBodyContent_mPagingControl_pageNumberLabel')
-    if page_label.nil?
-      # If we can't find the label assume there is only one page of results
-      1
-    elsif page_label.inner_text =~ /Page \d+ of (\d+)/
-      $~[1].to_i
-    else
-      raise "Unexpected form for number of pages"
-    end
-  end
-
   # Returns a list of URLs for all the applications on exhibition
-  def urls
+  def urls(scraper)
     # Get the main page and ask for the list of DAs on exhibition
     page = agent.get(enquiry_url)
     form = page.forms.first
     form.radiobuttons[1].click
     page = form.submit(form.button_with(:value => /Next/))
 
-    number_of_pages = extract_total_number_of_pages(page)
+    number_of_pages = scraper.extract_total_number_of_pages(page)
     urls = []
     (1..number_of_pages).each do |page_no|
       page = agent.get("EnquirySummaryView.aspx?PageNumber=#{page_no}")
@@ -55,7 +43,7 @@ class GoldCoastScraper
 
   def applications
     scraper = EpathwayScraper::Scraper.new("")
-    urls.map do |url|
+    urls(scraper).map do |url|
       # Get application page with a referrer or we get an error page
       page = agent.get(url, [], URI.parse(enquiry_url))
       data = scraper.scrape_detail_page(page)
