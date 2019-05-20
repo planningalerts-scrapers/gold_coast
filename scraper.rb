71d49518fafa255ed0f5bd4114c2016ec0ef661f
Matthew Landauer <mlandauer@gmail.com>
2019-05-20 15:42:06 +1000

    Use the library

--- scraper.rb
+++ scraper.rb
@@ -1,11 +1,10 @@
 require "epathway_scraper"
 
-scraper = EpathwayScraper::Scraper.new("https://cogc.cloud.infor.com/ePathway/epthprod")
-page = scraper.pick_type_of_search(:advertising)
+scraper = EpathwayScraper::Scraper.new(
+  "https://cogc.cloud.infor.com/ePathway/epthprod"
+)
 
-number_of_pages = scraper.extract_total_number_of_pages(page)
-
-scraper.scrape_all_index_pages_with_gets(number_of_pages) do |record|
+scraper.scrape(list_type: :advertising, with_gets: true) do |record|
   # Throw away the first part of the address which contains lot number
   record["address"] = record["address"].split(", ")[1..-1].join(", ")
   EpathwayScraper.save(record)
