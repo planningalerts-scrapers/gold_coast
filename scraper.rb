a9f509cbb854089adc3654f63e17c84b63be3f9d
Matthew Landauer <mlandauer@gmail.com>
2019-06-01 08:33:00 +1000

    Update to latest test data + upgrade gem to latest

--- scraper.rb
+++ scraper.rb
@@ -1,10 +1,9 @@
 require "epathway_scraper"
 
-scraper = EpathwayScraper::Scraper.new(
-  "https://cogc.cloud.infor.com/ePathway/epthprod"
-)
-
-scraper.scrape(list_type: :advertising, with_gets: true) do |record|
+scraper = EpathwayScraper.scrape(
+  "https://cogc.cloud.infor.com/ePathway/epthprod",
+  list_type: :advertising
+) do |record|
   # Throw away the first part of the address which contains lot number
   record["address"] = record["address"].split(", ")[1..-1].join(", ")
   EpathwayScraper.save(record)
