52f6a98f83e713e1ea08e0d30f60563ccd3c636d
Matthew Landauer <mlandauer@gmail.com>
2019-06-05 11:59:33 +1000

    Upgrade to latest version of the gem

--- scraper.rb
+++ scraper.rb
@@ -2,5 +2,5 @@ require "epathway_scraper"
 
 scraper = EpathwayScraper.scrape_and_save(
   "https://cogc.cloud.infor.com/ePathway/epthprod",
-  list_type: :advertising
+  list_type: :advertising, state: "QLD"
 )
